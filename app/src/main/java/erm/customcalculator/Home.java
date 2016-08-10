ea996ce28a645782a7f87177dc07392a80ea5948
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2016-08-10 12:37:25 -0500

    release

--- app/src/main/java/erm/customcalculator/Home.java
+++ app/src/main/java/erm/customcalculator/Home.java
@@ -23,7 +23,7 @@ import erm.customcalculator.fragments.CalcBase;
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    //CALC-3
+    //CALC-3 release 
 
     public final static String CURRENT_FRAGMENT_TAG = "current_fragment";
     FrameLayout mContent;
