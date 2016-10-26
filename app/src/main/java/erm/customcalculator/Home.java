b310662e85bbc976d28ff5731aa7e8b440a651e7
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2016-10-26 10:56:37 -0500

    removing excess !

--- app/src/main/java/erm/customcalculator/Home.java
+++ app/src/main/java/erm/customcalculator/Home.java
@@ -23,7 +23,7 @@ import erm.customcalculator.fragments.CalcBase;
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    //My Changes for my feature!!!!
+    //My Changes for my feature!
 
     public final static String CURRENT_FRAGMENT_TAG = "current_fragment";
     FrameLayout mContent;
